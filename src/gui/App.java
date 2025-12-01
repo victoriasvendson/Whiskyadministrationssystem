efc35822422e4a007adc412166284de5e9937089
ScraXie <alexandergbn@gmail.com>
2025-12-01 10:16:30 +0100

    2

--- src/gui/App.java
+++ src/gui/App.java
@@ -33,15 +33,12 @@ public class App {
 
     public static void initStorage() {
         // Objekter af vores lagerstyring
-        Hylde hyldeA10001 = Controller.opretHylde("00-01");
-        Hylde hyldeA10002 = Controller.opretHylde("00-02");
-        Hylde hyldeA10003 = Controller.opretHylde("00-03");
-        Hylde hyldeA11001 = Controller.opretHylde("10-01");
-        Hylde hyldeA11002 = Controller.opretHylde("10-02");
-        Hylde hyldeA11003 = Controller.opretHylde("10-03");
-        Hylde hyldeA20001 = Controller.opretHylde("00-01");
-        Hylde hyldeA20002 = Controller.opretHylde("00-02");
-        Hylde hyldeA20003 = Controller.opretHylde("00-03");
+        Hylde hyldeA11 = Controller.opretHylde("1");
+        Hylde hyldeA12 = Controller.opretHylde("2");
+        Hylde hyldeA13 = Controller.opretHylde("3");
+        Hylde hyldeA21 = Controller.opretHylde("1");
+        Hylde hyldeA22 = Controller.opretHylde("2");
+        Hylde hyldeA23 = Controller.opretHylde("3");
 
         Lager hammelLager = Controller.opretLager("Vestagervej 1, 8450 Hammel", 135);
         Lager randersLager = Controller.opretLager("Metervej 6, 8940 Randers SV", 300);
@@ -49,19 +46,15 @@ public class App {
         Reol reolA1 = Controller.opretReol("A1");
         Reol reolA2 = Controller.opretReol("A2");
 
-        reolA1.addHylde(hyldeA10001);
-        reolA1.addHylde(hyldeA10002);
-        reolA1.addHylde(hyldeA10003);
-        reolA1.addHylde(hyldeA11001);
-        reolA1.addHylde(hyldeA11002);
-        reolA1.addHylde(hyldeA11003);
-        reolA2.addHylde(hyldeA20001);
-        reolA2.addHylde(hyldeA20002);
-        reolA2.addHylde(hyldeA20003);
+        reolA1.addHylde(hyldeA11);
+        reolA1.addHylde(hyldeA12);
+        reolA1.addHylde(hyldeA13);
+        reolA2.addHylde(hyldeA21);
+        reolA2.addHylde(hyldeA22);
+        reolA2.addHylde(hyldeA23);
 
         hammelLager.addReol(reolA1);
         hammelLager.addReol(reolA2);
-
 
         // Objekter af leverandører og fade
         Leverandør glenfiddich = Controller.opretLeverandør("Glenfiddich", "glenfiddich@gmail.com",
@@ -80,7 +73,5 @@ public class App {
         Fad sherry4 = Controller.opretFad(5, 4, 200, tidligereIndhold1, "Italien", false, faryLochan);
         Fad portBarrel = Controller.opretFad(6, 14, 200, tidligereIndhold1, "Irland", true, glenfiddich);
         Fad mezcal = Controller.opretFad(7, 8, 100, tidligereIndhold1, "Mexico", true, glenfiddich);
-        //hh
-        //hh
     }
 }
