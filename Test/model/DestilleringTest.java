9d0fd676d61b1e9175e33697d5db4e6a8d5fd34f
ScraXie <alexandergbn@gmail.com>
2025-12-11 08:49:05 +0100

    gui på lagring

---
+++ Test/model/DestilleringTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DestilleringTest {
+
+    @org.junit.jupiter.api.Test
+    void getDestilleringsId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSlutDato() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setErFærdig() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMalt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
